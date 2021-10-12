db50410df7252610f455d7770c590f0aa276b667
Evhen <evgeniy.basiuk@gmail.com>
2021-10-12 22:12:05 +0300

    Initial commit

---
+++ app/src/main/java/com/ratiug/piechartexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ratiug.piechartexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ratiug.piechartexample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/ratiug/piechartexample/ui/main/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.ratiug.piechartexample.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ratiug.piechartexample.R
+
+class MainFragment : Fragment(R.layout.main_fragment) {
+ private lateinit var viewModel: MainViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+    }
+
+
+}
